d56004ce940058b7a165298bedf7d42bd823717b
gandharv <gandharvkumargarg@gmail.com>
2020-02-13 21:10:18 +0530

    [font] add roboto

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { DrizzleProvider } from '@drizzle/react-plugin'
 import { LoadingContainer } from '@drizzle/react-components'
 
-// import './App.css'
+import './app.css'
 
 import drizzleOptions from './drizzle-options'
 import Container from './container'
