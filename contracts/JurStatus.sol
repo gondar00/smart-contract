b948888990b70c58441fbba1f8eb8355fc07e6c9
gandharv <gandharvkumargarg@gmail.com>
2020-02-15 17:59:21 +0530

    [rewire] + add type struct

--- contracts/JurStatus.sol
+++ contracts/JurStatus.sol
@@ -1,28 +1,30 @@
 pragma solidity >=0.5.1 <0.7.0;
 
 contract JurStatus {
-    address owner;
-    string[] public statusTypes;
-    mapping(address => Status) public status;
-
-    /**
-      Struct defining the values of a status.
-      timestamp - The timestamp from which the status will be valid
-      active - booleans state depicting if a status is currently active
-      statusType - The type of the status complementing the bussiness logic
-    */
+    // Represents a single status.
     struct Status {
-      uint timestamp;
-      bool active;
-      string statusType;
+      uint timestamp; // timestamp from which the status will be valid
+      bool active; // boolean state depicting if a status is currently active
+      bytes32 statusType; // the type of the status
     }
 
-    /** Total count of Jur Statuses. */
-    // uint public statusCount;
+    // This is a type for a single status-type.
+    struct Type {
+      bytes32 name; // short name (up to 32 bytes)
+    }
 
-    event StatusTypeAdded(uint timestamp, string statusType);
-    event StateChanged(uint timestamp, address statusHolder, bool newState);
-    event StatusAdded(uint timestamp, address statusHolder, string statusType);
+    address owner;
+
+    // This declares a state variable - status that
+    // stores a `Status` struct for each possible address.
+    mapping(address => Status) public status;
+
+    // A dynamically-sized array of `Type` structs.
+    Type[] public statusTypes;
+
+    event TriggerStatusTypeAdded(uint timestamp, bytes32 statusType, string notification);
+    event TriggerStateChanged(uint timestamp, address statusHolder, bool newState, string notification);
+    event TriggerStatusAdded(uint timestamp, address statusHolder, uint position, string notification);
 
     modifier onlyOwner() {
       require(msg.sender == owner, "Access not granted.");
@@ -34,55 +36,45 @@ contract JurStatus {
       owner = msg.sender;
     }
 
-    /**
-      @dev addStatus - function to let the Jur admin add new address as a Jur Status, thus
-      validating it's license which can be verified on the blockchain.
-      @param _statusHolder - The address which will hold the Jur Status.
-      @param _position - The position of the value from the statusTypes array signifying the
-      status type.
-    */
+    // @dev addStatus - function to let the admin add new address as struct Status
     function addStatus(
-      address _statusHolder,
-      uint _position
+      address _statusHolder, // @param _statusHolder - The address which will hold the Jur Status.
+      uint _position // @param _position - The position of the value from the statusTypes array
     ) public
     onlyOwner {
       require(_statusHolder != address(0), "Please provide a valid address.");
-      require(_position >= statusTypes.length, "Position does not exist.");
-      status[_statusHolder] = Status(now, true, statusTypes[_position]);
-      // statusCount++;
+      status[_statusHolder] = Status(now, true, statusTypes[_position].name);
 
-      emit StatusAdded(now, _statusHolder, statusTypes[_position]);
+      emit TriggerStatusAdded(now, _statusHolder, _position, "Status added");
     }
 
-    /**
-      @dev changeState - function to let the Jur admin change the state of a status.
-      @param _statusHolder - The address holding the Jur Status.
-      @param _newState -  Boolean status to update.
-    */
+    // @dev changeState - function to let the Jur admin change the state of a status.
     function changeState(
-      address _statusHolder,
-      bool _newState
+      address _statusHolder, // @param _statusHolder - The address holding the Jur Status.
+      bool _newState // @param _newState -  Boolean status to update.
     ) public
     onlyOwner {
       require(_statusHolder != address(0), "Please provide a valid address.");
-      // require(status[_statusHolder].timestamp != 0, "Address is not a Jur Status holder.");
+      // Since `status` is a reference, this
+      // modifies `status[_statusHolder].active`
       require(status[_statusHolder].active != _newState, "Already in the same state.");
       status[_statusHolder].active = _newState;
 
-      emit StateChanged(now, _statusHolder, _newState);
+      emit TriggerStateChanged(now, _statusHolder, _newState, "Active state changed");
     }
 
-    /**
-      @dev addStatusType - function to let an admin add status types
-      @param _statusType - The new status type.
-    */
+    // @dev addStatusType - function to let an admin add status type
     function addStatusType(
-      string memory _statusType
+      bytes32 _statusType // @param _statusType - The new status type.
     ) public
     onlyOwner {
-      // require(bytes(_statusType).length == 0, "Status type cannot be an empty string.");
-      statusTypes.push(_statusType);
+      require(_statusType.length == 0, "Status type cannot be an empty string.");
 
-      emit StatusTypeAdded(now, _statusType);
+      // `Type({...})` creates a temporary
+      // Type object and `statusTypes.push(...)`
+      // appends it to the end of `statusTypes`.
+      statusTypes.push(Type(_statusType));
+
+      emit TriggerStatusTypeAdded(now, _statusType, "Status type added");
     }
 }
